e345a48ac06b3417f22d46ad064a1e7d64d95952
”sweet-and-sour24” <sweet.and.sour24@icloud.com>
2024-08-16 10:42:22 +0900

    3回目

--- typing/main.js
+++ typing/main.js
@@ -1,4 +1,36 @@
-const createText = () => {};
+let untyped = '';
+
+const untypedfield = document.getElementById('untyped');
+
+const textLists = [
+  // 'Hello World',
+  // 'This is my App',
+  // 'How are you?',
+  'Hello World','This is my App','How are you?',
+   'Today is sunny','I love JavaScript!','Good morning',
+   'I am Japanese','Let it be','Samurai',
+   'Typing Game','Information Technology',
+   'I want to be a programmer','What day is today?',
+   'I want to build a web app','Nice to meet you',
+   'Chrome Firefox Edge Safari','machine learning',
+   'Brendan Eich','John Resig','React Vue Angular',
+   'Netscape Communications','undefined null NaN',
+   'Thank you very much','Google Apple Facebook Amazon',
+   'ECMAScript','console.log','for while if switch',
+   'var let const','Windows Mac Linux iOS Android',
+   'programming'
+];
+
+const createText = () => {
+  // console.log(Math.floor(Math.random() * textLists.length));
+  let random = Math.floor(Math.random() * textLists.length);
+
+  // untyped = textLists[1];
+  untyped = textLists[random];
+  untypedfield.textContent = untyped;
+};
+
+createText();
 
 const keyPress = e => {};
 
